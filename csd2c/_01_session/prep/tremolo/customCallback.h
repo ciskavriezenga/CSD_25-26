aabf945039ac237f4e4c1cc75f1d2dc6b121c431
Ciska Vriezenga <ciskavriezenga@gmail.com>
2026-01-26 16:52:49 +0100

    done with lib

---
+++ csd2c/_01_session/prep/tremolo/customCallback.h
@@ -0,0 +1,35 @@
+//
+// Created by Ciska Vriezenga on 26/01/2026.
+//
+
+#ifndef CUSTOMCALLBACK_H
+#define CUSTOMCALLBACK_H
+
+#include "audiocomponent.h"
+#include "sine.h"
+#include "settings.h"
+#include <iostream>
+#include <random>
+
+
+class CustomCallback : public AudioCallback {
+public:
+    explicit CustomCallback (double Fs,
+                                int startMs = 0,
+                                int endMs = 0,
+                                const std::string& fileName = "0") : AudioCallback(Fs,  startMs, endMs, fileName){}
+    ~CustomCallback() override{}
+
+    void prepare (int sampleRate) override;
+
+    void process (AudioBuffer buffer) override;
+
+private:
+    std::random_device randomDevice;
+    std::mt19937 generator{randomDevice()};
+    std::uniform_real_distribution<> distribution{-1.0, 1.0};
+    Sine sine;
+};
+
+
+#endif //CUSTOMCALLBACK_H
